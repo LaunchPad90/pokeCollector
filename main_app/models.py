7e63df37a5a66a37d4bcf4b16b54ae2c7d0a56e6
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-03-17 21:43:25 -0600

    index view

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=20)
+    e_type = models.CharField(max_length=20)
+    description = models.TextField(max_length=250)
+    generation = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# from .models import Pokemon, pokemon
 # Create your views here.
+
+class Pokemon:
+    def __init__(self, name, e_type, description, generation):
+        self.name = name
+        self.e_type = e_type
+        self.description = description
+        self.generation = generation
+
+pokemon = [
+    Pokemon('Pikachu', 'Electric', 'Hates pokeballs, cute as a button', 1),
+    Pokemon('Squirtle', 'Water', 'Hard shell', 1),
+    Pokemon('Charmander', 'Fire', 'Dragon looking', 1)
+]
+
 def home(request):
     return HttpResponse('Hello PokeCollector')
 
 def about(request):
     return render(request, 'about.html')
+
+def pokemon_index(request):
+    return render(request, 'pokemon/index.html', { 'pokemon': pokemon })
