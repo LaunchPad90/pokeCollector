c48942252ccb3e34eea9b53ed0866ad960d7ecae
Jacob Hickey <launchpad@Jacobs-MacBook-Pro.local>
2020-03-18 16:47:23 -0600

    details page

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from .models import Pokemon, pokemon
+from .models import Pokemon
 # Create your views here.
 
-class Pokemon:
-    def __init__(self, name, e_type, description, generation):
-        self.name = name
-        self.e_type = e_type
-        self.description = description
-        self.generation = generation
-
-pokemon = [
-    Pokemon('Pikachu', 'Electric', 'Hates pokeballs, cute as a button', 1),
-    Pokemon('Squirtle', 'Water', 'Hard shell', 1),
-    Pokemon('Charmander', 'Fire', 'Dragon looking', 1)
-]
-
 def home(request):
-    return HttpResponse('Hello PokeCollector')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def pokemon_index(request):
+    pokemon = Pokemon.objects.all()
     return render(request, 'pokemon/index.html', { 'pokemon': pokemon })
+
+def pokemon_detail(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokemon/detail.html', {'pokemon': pokemon})
